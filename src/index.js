38ca33f6e0e2f4a101c8e33308d95438ea954943
gasz <1pawsao1@gmail.com>
2022-04-05 02:17:30 +0200

    Compiling SASS to one CSS file - index.css Removing SCSS files

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// import './index.css';
-import './style/variables.css';
-import './style/index.css';
-import './style/about.css';
-import './style/navbar.css';
-import './style/home.css';
-import './style/loading.css';
-import './style/searchForm.css';
-import './style/singleCocktail.css';
-import './style/cocktailList.css';
-import './style/error.css';
-import './style/cocktail.css';
-
 import App from './App';
 import { AppProvider } from './context';
+import './index.css';
 
 ReactDOM.render(
     <React.StrictMode>
@@ -25,16 +13,14 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById('root')
 );
-// swDev();
-// if ('serviceWorker' in navigator) {
-//     window.addEventListener('load', () => {
-//         navigator.serviceWorker
-//             .register('./serviceWorker.js')
-//             .then((reg) => {
-//                 console.log('działa', reg);
-//             })
-//             .catch((err) => {
-//                 console.log('Error: ', err);
-//             });
-//     });
-// }
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('./serviceWorker.js')
+            .then((reg) => {})
+            .catch((err) => {
+                console.log('Error: ', err);
+            });
+    });
+}
